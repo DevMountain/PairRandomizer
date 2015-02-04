f8b9c8db5104647c7d7a25d71bb1eb898ad6638b
Caleb Hicks <calebhicks@gmail.com>
2015-02-04 13:54:57 -0700

    adds collection view and configures cell

---
+++ PairRandomizer/StudentCollectionViewCell.h
@@ -0,0 +1,15 @@
+//
+//  StudentCollectionViewCell.h
+//  PairRandomizer
+//
+//  Created by Caleb Hicks on 2/4/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StudentCollectionViewCell : UICollectionViewCell
+
+@property (weak, nonatomic) IBOutlet UILabel *textLabel;
+
+@end
---
+++ PairRandomizer/StudentCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  StudentCollectionViewCell.m
+//  PairRandomizer
+//
+//  Created by Caleb Hicks on 2/4/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import "StudentCollectionViewCell.h"
+
+@implementation StudentCollectionViewCell
+
+@end
