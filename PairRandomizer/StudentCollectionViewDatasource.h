50f293f6db3997085c1dfabb14361a5b40965f07
Caleb Hicks <calebhicks@gmail.com>
2015-02-04 13:28:44 -0700

    adds datasource class

---
+++ PairRandomizer/StudentCollectionViewDatasource.h
@@ -0,0 +1,13 @@
+//
+//  StudentCollectionViewDatasource.h
+//  PairRandomizer
+//
+//  Created by Caleb Hicks on 2/4/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface StudentCollectionViewDatasource : NSObject
+
+@end
---
+++ PairRandomizer/StudentCollectionViewDatasource.m
@@ -0,0 +1,13 @@
+//
+//  StudentCollectionViewDatasource.m
+//  PairRandomizer
+//
+//  Created by Caleb Hicks on 2/4/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import "StudentCollectionViewDatasource.h"
+
+@implementation StudentCollectionViewDatasource
+
+@end
